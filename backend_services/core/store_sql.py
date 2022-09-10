0d885ad71baa0596d2f94a4fea7e040debd6ee63
kathryn.cowie <kathryn.cowie@nested-knowledge.com>
2022-09-10 14:42:11 -0400

    testing sql con

--- backend_services/core/store_sql.py
+++ backend_services/core/store_sql.py
@@ -11,20 +11,23 @@ class SQLPipeline:
 
     def create_connection(self, erase_first=False):
         print("SQL connection created")
-        self.conn = sqlite3.connect(('data/{0}.db').format(self.db_name))
+        self.conn = sqlite3.connect(('{0}.db').format(self.db_name))
         db  = self.conn.cursor()
 
     def create_table(self):
         db = self.conn.cursor()
         db.execute("""DROP TABLE IF EXISTS patients""")
         db.execute("""CREATE TABLE patients
-        (PRIMARY KEY(id), first_name TEXT, last_name TEXT,
-        address TEXT, city TEXT, state TEXT, sales_count INT)
+        (id INTEGER, first_name TEXT, last_name TEXT,
+        address TEXT, city TEXT, state TEXT, sales_count INT, PRIMARY KEY(id))
         """)
+        print("patients table created")
         db.execute("""CREATE TABLE patient_sales
-        (PRIMARY KEY(id), FOREIGN key(patient_id) REFERENCES patients (id),
-        sales_count INT)
+        (id INT, patient_id INT
+        sales_count INT, PRIMARY KEY(id), FOREIGN KEY(patient_id) REFERENCES patients (id))
         """)
+        print("patient sales table created")
+
     def store_patient_data(self, patients_list, table_name):
         db = self.conn.cursor()
         if table_name == 'patient' or table_name == 'patients':
@@ -32,13 +35,13 @@ class SQLPipeline:
                 query = '''INSERT INTO patients (first_name, last_name, address, city, state) VALUES(?, ?, ?, ?, ?, ?)'''
                 data = (pat.first_name, pat.last_name, pat.address, pat.city, pat.state)
                 db.execute(query, data)
-                print(pat.last_name, "patient info was stored in table")
+                print(pat.last_name, ": patient info was stored in table")
 
     def store_patient_sales(self, patients_list, sales_list):
         db = self.conn.cursor()
         for pat in patients:
             db.execute('''INSERT INTO patient_sales (patient_id, sales_count) VALUES (?, ?)''', pat.id, sales_count)
 
-   def save_changes(self):
+    def save_changes(self):
         self.conn.commit()
         self.conn.close()
---
+++ backend_services/testcon.py
@@ -0,0 +1,18 @@
+import sqlite3
+import pandas as pd
+
+from core.store_sql import SQLPipeline
+
+# Set up SQL connection and Store patient identifier information
+sqlcon = SQLPipeline()
+try:
+    sqlcon.create_connection()
+except Exception as ex:
+         print("Exception:", ex)
+
+sqlcon.create_table()     
+# sqlcon.store_data(patients, 'patient')
+#
+#
+# finally:
+#     SQLCon.save_changes()
